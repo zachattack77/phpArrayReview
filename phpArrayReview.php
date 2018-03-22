fae131d6a526059dbe37f8f2e03d1490a3e2d124
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-22 16:23:25 -0400

    defined array

---
+++ phpArrayReview.php
@@ -0,0 +1,13 @@
++<?php
++/**
++ * Created by PhpStorm.
++ * User: zach
++ * Date: 2/22/2018
++ */
++
++$animals = array("panda", "alpaca", "boa");
++
++function printAnimals($animals)
++{
+    +
+    +}
