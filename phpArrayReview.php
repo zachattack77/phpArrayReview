3993b91f7bf874f60ea8c1fb834a04790e3a05f8
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-22 16:28:31 -0400

    final commit

--- phpArrayReview.php
+++ phpArrayReview.php
@@ -1,21 +1,43 @@
-+<?php
-+/**
-+ * Created by PhpStorm.
-+ * User: zach
-+ * Date: 2/22/2018
-+ */
-+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2/22/2018
+ */
+$animals = array("panda", "alpaca", "boa");
+function printAnimals($animals)
+{
+    sort($animals);
+    for($i = 0; $i < count($animals); $i++)
+    {
+        echo $animals[$i] . " ";
+    }
+}
+function addAnimals($string, $animals)
+{
+    echo nl2br ("\n adding $string ... \n");
+    $string = strtolower($string);
+    if(!(in_array($string, $animals)))
+    {
+        array_push($animals, $string);
+    }
+    printAnimals($animals);
+}
 printAnimals($animals);
 addAnimals("goat", $animals);
 addAnimals("Boa", $animals);
-+
-+
-+$flavors = array(
-    +    "grasshopper"=>"The Grasshopper",
-    +    "maple"=>"Whiskey Maple Bacon",
-    +    "carrot"=>"Carrot Walnut",
-    +    "caramel"=>"Salted Caramel Cupcake",
-    +    "velvet"=>"Red Velvet",
-    +    "lemon"=>"Lemon Drop",
-    +    "tiramisu"=>"Tiramisu",
-    +); imals("Boa", $animals);ntAnimals($animals);
+$flavors = array(
+    "grasshopper"=>"The Grasshopper",
+    "maple"=>"Whiskey Maple Bacon",
+    "carrot"=>"Carrot Walnut",
+    "caramel"=>"Salted Caramel Cupcake",
+    "velvet"=>"Red Velvet",
+    "lemon"=>"Lemon Drop",
+    "tiramisu"=>"Tiramisu",
+);
+echo nl2br ("\n\n\n");
+foreach ($flavors as $flavor => $name)
+{
+    echo "<input type=\"checkbox\" name=\"flavors[]\" value=\"$flavor\"> $name<br>";
+}
