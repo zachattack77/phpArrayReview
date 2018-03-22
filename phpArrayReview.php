e378601f876125eff7baf454df63cf718c7ca759
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-22 16:26:01 -0400

    added animal function

--- phpArrayReview.php
+++ phpArrayReview.php
@@ -5,14 +5,17 @@
 + * Date: 2/22/2018
 + */
 +
- function printAnimals($animals)
- {
-     +    sort($animals);
-
-     -}
-+    for($i = 0; $i < count($animals); $i++)
-    +    {
-    +        echo $animals[$i] . " ";
-+    }
++function addAnimals($string, $animals)
++{
+    +    echo nl2br ("\n adding $string ... \n");
++    $string = strtolower($string);
++    if(!(in_array($string, $animals)))
+        +    {
+        +        array_push($animals, $string);
+        +    }
++    printAnimals($animals);
 +}
++
 +printAnimals($animals);
++addAnimals("goat", $animals);
++addAnimals("Boa", $animals);ntAnimals($animals);
