830a75ca52b13cd2edf47fad7b4b703410d24437
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-22 16:27:08 -0400

    define array

--- phpArrayReview.php
+++ phpArrayReview.php
@@ -5,17 +5,17 @@
 + * Date: 2/22/2018
 + */
 +
-+function addAnimals($string, $animals)
-+{
-    +    echo nl2br ("\n adding $string ... \n");
-+    $string = strtolower($string);
-+    if(!(in_array($string, $animals)))
-        +    {
-        +        array_push($animals, $string);
-        +    }
-+    printAnimals($animals);
-+}
+printAnimals($animals);
+addAnimals("goat", $animals);
+addAnimals("Boa", $animals);
 +
-+printAnimals($animals);
-+addAnimals("goat", $animals);
-+addAnimals("Boa", $animals);ntAnimals($animals);
++
++$flavors = array(
+    +    "grasshopper"=>"The Grasshopper",
+    +    "maple"=>"Whiskey Maple Bacon",
+    +    "carrot"=>"Carrot Walnut",
+    +    "caramel"=>"Salted Caramel Cupcake",
+    +    "velvet"=>"Red Velvet",
+    +    "lemon"=>"Lemon Drop",
+    +    "tiramisu"=>"Tiramisu",
+    +); imals("Boa", $animals);ntAnimals($animals);
